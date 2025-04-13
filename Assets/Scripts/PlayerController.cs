d2600845b6c568d700de4b32f0ca1237dddb9a26
ejcaie <evancaie705@gmail.com>
2025-04-12 20:54:28 -0400

    Some things are starting to work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,35 +10,28 @@ public class PlayerController : MonoBehaviour
     public Quaternion leftRotationStart;
     public Quaternion rightRotationStart;
 
-    private Quaternion leftRotationTarget;
-    private Quaternion rightRotationTarget;
-
-    private void Start()
-    {
-        //leftRotationStart = leftFlipper.rotation;
-        //rightRotationStart = rightFlipper.rotation;
-}
+    public float rotateSpeed = 2f;
 
     void Update()
     {
         Quaternion leftTarget = Quaternion.Euler(0, 0, 80);
         if (Input.GetKey(KeyCode.A) && leftFlipper.rotation.z < leftTarget.z)
         {
-            leftFlipper.rotation = leftFlipper.rotation * Quaternion.Euler(0, 0, 0.8f);
+            leftFlipper.rotation = leftFlipper.rotation * Quaternion.Euler(0, 0, rotateSpeed);
         }
         else if (leftFlipper.rotation.z > 0)
         {
-            leftFlipper.rotation = leftFlipper.rotation * Quaternion.Euler(0, 0, -1);
+            leftFlipper.rotation = leftFlipper.rotation * Quaternion.Euler(0, 0, -rotateSpeed);
         }
 
         Quaternion rightTarget = Quaternion.Euler(0, 0, -80);
         if (Input.GetKey(KeyCode.D) && rightFlipper.rotation.z > rightTarget.z)
         {
-            rightFlipper.rotation = rightFlipper.rotation * Quaternion.Euler(0, 0, -0.8f);
+            rightFlipper.rotation = rightFlipper.rotation * Quaternion.Euler(0, 0, -rotateSpeed);
         }
         else if (rightFlipper.rotation.z < 0)
         {
-            rightFlipper.rotation = rightFlipper.rotation * Quaternion.Euler(0, 0, 1);
+            rightFlipper.rotation = rightFlipper.rotation * Quaternion.Euler(0, 0, rotateSpeed);
         }
     }
 }
